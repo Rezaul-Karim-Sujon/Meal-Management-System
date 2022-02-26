bfa3be61fe6d82e3ab3df200e1fb9f7f7b0f5334
Rezaul Karim <rksujon126706@gmail.com>
2022-02-26 09:27:29 +0600

    Company Info Api finished

--- Meal Management Web API/Controllers/CompanyInfoesController.cs
+++ Meal Management Web API/Controllers/CompanyInfoesController.cs
@@ -6,18 +6,24 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Meal_Management_Web_API.Models.Entities;
+using Meal_Management_Web_API.Models.ViewModel;
+using Meal_Management_Web_API.Models.Others;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Meal_Management_Web_API.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class CompanyInfoesController : ControllerBase
     {
         private readonly AppDbContext _context;
-
-        public CompanyInfoesController(AppDbContext context)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public CompanyInfoesController(AppDbContext context,IWebHostEnvironment webHostEnvironment)
         {
             _context = context;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         // GET: api/CompanyInfoes
@@ -45,13 +51,29 @@ namespace Meal_Management_Web_API.Controllers
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutCompanyInfo(int id, CompanyInfo companyInfo)
+        public async Task<IActionResult> PutCompanyInfo(int id, [FromForm]VMCompanyInfo filter)
         {
-            if (id != companyInfo.Id)
+            if (id != filter.Id)
             {
                 return BadRequest();
             }
-
+            var result = await _context
+                .CompanyInfos
+                .AsNoTracking()
+                .SingleOrDefaultAsync(e => e.Id == id);
+            string path = string.Concat(_webHostEnvironment.WebRootPath, Constant.CompanyLogoPath.NoTilde()) + result.Logo;
+            if (path != null && System.IO.File.Exists(path))
+            {
+                System.IO.File.Delete(path);
+            }
+            string PicturePath = UploadFileControl.FileName(filter.Logo,
+                string.Concat(_webHostEnvironment.WebRootPath, Constant.CompanyLogoPath.NoTilde()));
+            var companyInfo = new CompanyInfo
+            {
+                Id = id,
+                Name=filter.Name,
+                Logo=PicturePath
+            };
             _context.Entry(companyInfo).State = EntityState.Modified;
 
             try
@@ -70,15 +92,25 @@ namespace Meal_Management_Web_API.Controllers
                 }
             }
 
-            return NoContent();
+            return CreatedAtAction("GetCompanyInfo", new
+            {
+                id = companyInfo.Id
+            }, companyInfo);
         }
 
         // POST: api/CompanyInfoes
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [HttpPost]
-        public async Task<ActionResult<CompanyInfo>> PostCompanyInfo(CompanyInfo companyInfo)
+        public async Task<ActionResult<CompanyInfo>> PostCompanyInfo([FromForm]VMCompanyInfo filter)
         {
+            string PicturePath = UploadFileControl.FileName(filter.Logo,
+                string.Concat(_webHostEnvironment.WebRootPath, Constant.CompanyLogoPath.NoTilde()));
+            var companyInfo = new CompanyInfo
+            {
+                Name = filter.Name,
+                Logo = PicturePath
+            };
             _context.CompanyInfos.Add(companyInfo);
             await _context.SaveChangesAsync();
 
@@ -92,13 +124,25 @@ namespace Meal_Management_Web_API.Controllers
             var companyInfo = await _context.CompanyInfos.FindAsync(id);
             if (companyInfo == null)
             {
-                return NotFound();
+                return NotFound(new ResponseModel
+                {
+                    Success = false,
+                    Message = "Company Not found"
+                });
             }
-
+            string path = string.Concat(_webHostEnvironment.WebRootPath, Constant.CompanyLogoPath.NoTilde()) + companyInfo.Logo;
+            if (System.IO.File.Exists(path))
+            {
+                System.IO.File.Delete(path);
+            }
             _context.CompanyInfos.Remove(companyInfo);
             await _context.SaveChangesAsync();
 
-            return companyInfo;
+            return Ok(new ResponseModel
+            {
+                Success = true,
+                Message = "Company Successfully Deleted"
+            });
         }
 
         private bool CompanyInfoExists(int id)
---
+++ Meal Management Web API/Models/ViewModel/VMCompanyInfo.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Meal_Management_Web_API.Models.ViewModel
+{
+    public class VMCompanyInfo:VmBaseEntity
+    {
+        public string Name { get; set; }
+        public IFormFile Logo { get; set; }
+    }
+}
